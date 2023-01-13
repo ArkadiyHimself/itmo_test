83406fe6f3a889a31328c269c94f20f4509f3524
Arkabu <goodieplay20@gmail.com>
2023-01-13 18:01:15 +0300

    new decor function

--- task 1.py
+++ task 1.py
@@ -1,5 +1,14 @@
 def summ(a, b, c = 1, d = 3):
     return a**2 + b**2 + c**2 + d**2
+
+
+def three_times(a, b):
+    for i in range(1, 4):
+        result = summ(a, b)
+        print("Iteration ", i, ':', result)
+    return ""
+
+
 a = int(input())
 d = int(input())
-print(summ(a, d))
+print(three_times(a, d))
